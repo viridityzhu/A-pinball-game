ccce36f7c81a2a944d8265de16a13d1f8141eec8
朱珈印 <asdfg990518@163.com>
2019-04-12 20:22:47 +0800

    完成杆的初始化部分代码
    主要更改：
    1. 增加杆的初始化部分的代码
    2. 改变窗体大小、小球速度等，让游戏稍微刺激一点
    3. 增加按下关闭按钮退出程序的代码

--- PinBall.java
+++ PinBall.java
@@ -7,36 +7,43 @@ import java.awt.event.*;
 public class PinBall
 {
 	// 桌面的宽度
-	private final int TABLE_WIDTH = 300;
+	private final int TABLE_WIDTH = 1000;
 	// 桌面的高度
-	private final int TABLE_HEIGHT = 400;
+	private final int TABLE_HEIGHT = 800;
 	// 球拍的垂直位置
-	private final int RACKET_Y = 340;
+	private final int RACKET_Y = TABLE_HEIGHT-50;
 	// 下面定义球拍的高度和宽度
-	private final int RACKET_HEIGHT = 20;
-	private final int RACKET_WIDTH = 60;
+	private final int RACKET_HEIGHT = 30;
+	private final int RACKET_WIDTH = 200;
 	// 小球的大小
-	private final int BALL_SIZE = 16;
+	private final int BALL_SIZE = 24;
 	private Frame f = new Frame("弹球游戏");
 	Random rand = new Random();
 	// 小球纵向的运行速度
-	private int ySpeed = 10;
+	private int ySpeed = 30;
 	// 返回一个-0.5~0.5的比率，用于控制小球的运行方向。
 	private double xyRate = rand.nextDouble() - 0.5;
 	// 小球横向的运行速度
 	private int xSpeed = (int)(ySpeed * xyRate * 2);
 	// ballX和ballY代表小球的坐标
-	private int ballX = rand.nextInt(200) + 20;
-	private int ballY = rand.nextInt(10) + 20;
+	private int ballX = 100;
+	private int ballY = RACKET_Y-BALL_SIZE*2;
 	// racketX代表球拍的水平位置
-	private int racketX = rand.nextInt(200);
+	private int racketX = ballX+BALL_SIZE/2-RACKET_WIDTH/2;
 	private MyCanvas tableArea = new MyCanvas();
 	Timer timer;
 	// 游戏是否结束的旗标
 	private boolean isLose = false;
+	// 游戏是否开始的旗标
+	private boolean isStart = false;
 	public void init()
 	{
-		//f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//按下关闭按钮，退出程序
+		f.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 		// 设置桌面区域的最佳大小
 		tableArea.setPreferredSize(
 			new Dimension(TABLE_WIDTH , TABLE_HEIGHT));
@@ -50,12 +57,12 @@ public class PinBall
 				if (ke.getKeyCode() == KeyEvent.VK_LEFT)
 				{
 					if (racketX > 0)
-					racketX -= 10;
+					racketX -= 3*Math.abs(xSpeed);
 				}
 				if (ke.getKeyCode() == KeyEvent.VK_RIGHT)
 				{
 					if (racketX < TABLE_WIDTH - RACKET_WIDTH)
-					racketX += 10;
+					racketX += 3*Math.abs(xSpeed);
 				}
 			}
 		};
@@ -72,7 +79,7 @@ public class PinBall
 			}
 			// 如果小球高度超出了球拍位置，且横向不在球拍范围之内，游戏结束。
 			if (ballY >= RACKET_Y - BALL_SIZE &&
-				(ballX < racketX || ballX > racketX + RACKET_WIDTH))
+				(ballX < racketX-BALL_SIZE || ballX > racketX + RACKET_WIDTH+BALL_SIZE))
 			{
 				timer.stop();
 				// 设置游戏是否结束的旗标为true。
@@ -87,12 +94,33 @@ public class PinBall
 				ySpeed = -ySpeed;
 			}
 			// 小球坐标增加
-			ballY += ySpeed;
-			ballX += xSpeed;
+			if(isStart)
+			{
+				ballY += ySpeed;
+				ballX += xSpeed;
+			}
 			tableArea.repaint();
 		};
-		timer = new Timer(100, taskPerformer);
+		timer = new Timer(50, taskPerformer);
 		timer.start();
+		
+		// 添加鼠标事件监听器
+		tableArea.addMouseListener(new MouseAdapter (){
+			public void mousePressed(MouseEvent e) {
+				if(!isStart) {
+					isStart = true;
+				}
+			}
+		});
+		// 添加鼠标移动事件监听器
+		tableArea.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseMoved(MouseEvent e) {
+				if(!isStart) {
+					ballX=e.getX()-BALL_SIZE/2;
+					racketX=e.getX()-RACKET_WIDTH/2;
+				}
+			}
+		});
 		f.pack();
 		f.setVisible(true);
 	}
@@ -110,13 +138,13 @@ public class PinBall
 			{
 				g.setColor(new Color(255, 0, 0));
 				g.setFont(new Font("Times" , Font.BOLD, 30));
-				g.drawString("游戏已结束！" , 50 ,200);
+				g.drawString("游戏已结束！" , TABLE_WIDTH/2-80 ,TABLE_HEIGHT/2);
 			}
 			// 如果游戏还未结束
 			else
 			{
 				// 设置颜色，并绘制小球
-				g.setColor(new Color(240, 240, 80));
+				g.setColor(new Color(230, 230, 80));
 				g.fillOval(ballX , ballY , BALL_SIZE, BALL_SIZE);
 				// 设置颜色，并绘制球拍
 				g.setColor(new Color(80, 80, 200));
